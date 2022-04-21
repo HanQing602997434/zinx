4c98b146d662affd35395949317ec0963b19b640
hanqing <602997434@qq.com>
2022-04-21 01:11:25 -0700

    消息封装

---
+++ message.go
@@ -0,0 +1,17 @@
+
+// 消息封装
+/*
+	定义一个消息的结构体Message
+		属性：
+			消息ID
+			消息长度
+			消息的内容
+
+		方法：
+			GetMsgId() uint32
+			GetMsgLen() uint32
+			GetData() []byte
+			SetMsgId(uint32)
+			SetDataLen(uint32)
+			SetData([]byte)
+*/
