acc95615cebb97134a896586e134450b9764f189
hanqing <602997434@qq.com>
2022-05-05 20:26:39 -0700

    连接管理

--- connection_manager.go
+++ connection_manager.go
@@ -10,10 +10,15 @@
 
 		方法
 			添加连接
+				func (connMgr *ConnManager) Add(conn ziface.IConnection)
 			删除连接
+				func (connMgr *ConnManager) Remove(conn ziface.IConnection)
 			根据连接ID查找对应的连接
+				func (connMgr *ConnManager) Get(connID uint32) (ziface.IConnection, error)
 			总连接个数
+				func (connMgr *ConnManager) Len() int
 			清理全部的连接
+				func (connMgr *ConnManager) ClearConn()
 	
 	将连接管理模块集成到zinx框架中
 
